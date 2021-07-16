0e13b6a1ca8d035b7ab830c16c93bd52f1f3273b
Prokopuk Pavel <pashaprok23@gmail.com>
2021-07-16 08:54:52 +0300

    email mailtrap

--- utils/email.js
+++ utils/email.js
@@ -13,11 +13,21 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
+      // return nodemailer.createTransport({
+      //   service: 'SendGrid',
+      //   auth: {
+      //     user: process.env.SENDGRID_USERNAME,
+      //     pass: process.env.SENDGRID_PASSWORD,
+      //   },
+      // });
+
+
       return nodemailer.createTransport({
-        service: 'SendGrid',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
         },
       });
     }
